e2a9255810a1bcb44de18702c96e67f7052459db
NathanVieira59 <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2024-07-23 12:31:46 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,107 @@
-import './App.css'
+import './App.css';
+
+//DECLARADAS Ou NOMEADAS
+console.log('saudacao: ', saudacao('Bora Bill')); // Saída: Olá, mundo!
+
+function saudacao(nome) {
+    return `Olá, ${nome}!`;
+}
+
+//EXPRESSAS
+//oiMundo(); //Uncaught ReferenceError: Cannot access 'oiMundo' before initialization"
+
+const oiMundo = function() {
+    console.log('oiMundo: ', "Oi, mundo!");
+};
+
+//oiMundo(); // Saída: Oi, mundo!
+
+//Declarada
+function add(x, y) {  return x + y;}
+console.log("Declarada: ", add(2,2)); //4
+//Anonima
+console.log("Anonima: ", [2, 2].reduce(function (x, y) { return x + y; })); //4
+//Arrow 
+let myAdd = (x, y) => x + y;
+console.log("Arrow: ", myAdd(2,2)); //4
+
+
+//CONSTRUTORAS
+function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+}
+
+const pessoa1 = new Pessoa("João", 25);
+const pessoa2 = new Pessoa("Maria", 30);
+
+console.log("pessoa1: ", pessoa1); // Saída: Pessoa { nome: 'João', idade: 25 }
+console.log("pessoa2: ", pessoa2); // Saída: Pessoa { nome: 'Maria', idade: 30 }
+
+//RECURSIVAS
+function fatorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * fatorial(n - 1);
+  }
+}
+
+console.log("fatorial: ",fatorial(5)); // Saída: 120
+
+
+//ASYNC || Assíncronas
+async function myAsyncFunction() {
+  let response = await fetch('https://fakestoreapi.com/products/1');
+  let data = await response.json();
+  return(data);
+}
+console.log(myAsyncFunction());
+
+//GENERATOR
+function* iterateProperties(obj) {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      yield key;
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        yield* iterateProperties(obj[key]);
+      }
+    }
+  }
+}
+
+
+// Objeto com função 
+const persona = {
+  name: "Object with function",
+  hello: function() {
+    console.log(`Hello, I am ${this.name}!`);
+  }
+}
+
+// Call the method function
+persona.hello(); // Output: Hello, I am John Doe!
+
+//commented || unused
+/*
+async function main() {
+  const product = await myAsyncFunction();
+  for (const key of iterateProperties(product)) {
+    console.log(key, product[key]);
+  }
+}
+
+main();
+*/
+
+document.querySelector("#app").innerHTML = saudacao('Infnet');
+
+
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
+    <h1>
+     vh stringo
+    </h1>
   )
 }
---
+++ src/fundamentos.jsx
@@ -0,0 +1,53 @@
+//mutavel
+let aula = "aula1";
+console.log(aula);
+
+aula = "aula2";
+console.log(aula);
+
+//imutavel
+const faculdade = "UBM";
+console.log(faculdade);
+
+//booleano
+let isAluno = true;
+console.log(isAluno);
+
+
+//operadores
+
+let sum = 5+9;
+console.log(sum)
+
+
+//object
+const pessoa = { nome:"Bora Bill", idade:999}
+console.log(pessoa)
+
+console.log(`hello ${pessoa.nome}`)
+
+//array
+const frutas = ['pera','goiaba'];
+const numeros = [1,2,3,4,5];
+ console.log(frutas);
+ console.log(numeros);
+
+
+
+
+//condicionais
+if (isAluno){
+  console.log("Receba")
+}else(
+  console.log("leeeeepo")
+)
+
+
+
+
+//funcoes
+
+
+
+
+
